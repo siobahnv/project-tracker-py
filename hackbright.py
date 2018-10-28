1ae0f1a4030a9d1a876977e8d3ee4c3321449094
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-27 17:00:17 -0700

    Finish project-tracker-py

--- hackbright.py
+++ hackbright.py
@@ -58,17 +58,50 @@ def make_new_student(first_name, last_name, github):
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
-    pass
+    
+    QUERY = """
+        SELECT title, description, max_grade
+        FROM projects
+        WHERE title = :title
+        """
+
+    db_cursor = db.session.execute(QUERY, {'title': title})
+
+    row = db_cursor.fetchone()
+
+    print("Title: {}\nDescription: {}\nMax Grade: {}".format(row[0], row[1], row[2]))
 
 
 def get_grade_by_github_title(github, title):
     """Print grade student received for a project."""
-    pass
+    
+    QUERY = """
+    	SELECT project_title, grade
+    	FROM grades
+    	WHERE student_github = :github 
+    		AND project_title = :title
+    	"""
+
+    db_cursor = db.session.execute(QUERY, {'github': github, 'title': title})
+
+    row = db_cursor.fetchone()
+
+    print("Title: {}\nGrade: {}".format(row[0], row[1]))
 
 
 def assign_grade(github, title, grade):
     """Assign a student a grade on an assignment and print a confirmation."""
-    pass
+    QUERY = """
+    	INSERT INTO grades (student_github, project_title, grade)
+    	VALUES (:github, :title, :grade)
+    	"""
+
+    db.session.execute(QUERY, {'github': github,
+    							'title': title,
+    							'grade': grade})
+    db.session.commit()
+
+    print(f"Successfully assigned grade: {grade} for {github} in {title}.")
 
 
 def handle_input():
@@ -93,6 +126,18 @@ def handle_input():
         elif command == "new_student":
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
+
+        elif command == "project":
+            title = args[0]
+            get_project_by_title(title)
+
+        elif command == "get_grade":
+        	github, title = args
+        	get_grade_by_github_title(github, title)
+
+        elif command == "assign_grade":
+        	github, title, grade = args
+        	assign_grade(github, title, grade)
 
         else:
             if command != "quit":
